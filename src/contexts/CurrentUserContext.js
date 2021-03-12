79bfeb96a14bfe577fd279a9c65408eb4f3b5126
Guseva Anastasia <kaa16080709@yandex.ru>
2021-03-12 18:08:18 +0300

    Updates

--- src/contexts/CurrentUserContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const CurrentUserContext = React.createContext({}); //создаём новый объект контекста
-
-export default CurrentUserContext;
--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  url: 'https://nomoreparties.co/cohort11',
-  headers: {
-    authorization: 'e7c47a46-a800-41a0-9207-658ce4c95ce5',
-    'Content-Type': 'application/json',
-  },
-};
-
-export default config;
